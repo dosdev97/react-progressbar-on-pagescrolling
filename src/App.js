7b2e7d0b6575b497969ba05938e81d1781417835
Donald Silver <donaldsilver97@gmail.com>
2019-12-13 22:39:25 +0300

    added some intial files. public/index.html src/index.js src/App.js, App.css

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header></header>
+        <main>
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            ...more paragraphs of text, enough so that the page gets a scrollbar
+          </p>
+        </main>
+      </div>
+    );
+  }
+}
